4f3b97f6947d82d6b39913e259259b1b701767f5
Igor <malysh_ii@nlmk.com>
2024-12-03 17:21:30 +0300

    wip

--- kubernetes_app/dataprocessing/app.py
+++ kubernetes_app/dataprocessing/app.py
@@ -1,11 +1,17 @@
+import httpx
+import pandas as pd
+from io import BytesIO
+from uuid import uuid4
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import StreamingResponse
 from enum import Enum
 import uvicorn
-from fastapi import FastAPI, UploadFile, HTTPException
-from fastapi.responses import StreamingResponse
-import pandas as pd
-import io
 
 app = FastAPI(title="Data Processor API", version="1.0.0", description="API for processing datasets.")
+
+
+DB_SERVICE_URL = "http://db-service-service:8080"
+DB_SERVICE_URL = "http://192.168.49.2:32020"
 
 
 class AnonymizationType(str, Enum):
@@ -21,22 +27,20 @@ class TransformationType(str, Enum):
     Capitalize = "Capitalize"
 
 
-# Helper function to read uploaded dataset
-def read_dataset(file: UploadFile):
+# Helper function to download dataset from URL
+async def download_dataset(url: str):
     try:
-        if file.filename.endswith(".csv"):
-            return pd.read_csv(io.BytesIO(file.file.read()))
-        elif file.filename.endswith(".json"):
-            return pd.read_json(io.BytesIO(file.file.read()))
-        else:
-            raise HTTPException(status_code=400, detail="Unsupported file format. Only CSV and JSON are supported.")
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+            response.raise_for_status()  # Ensure the request is successful
+            return pd.read_csv(BytesIO(response.content))  # Assuming CSV for simplicity
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Error reading dataset: {str(e)}")
+        raise HTTPException(status_code=400, detail=f"Error downloading dataset: {str(e)}")
 
 
 # Helper function to return a DataFrame as an in-memory file
 def dataframe_to_memory_file(df: pd.DataFrame, file_format: str = "csv"):
-    memory_file = io.BytesIO()
+    memory_file = BytesIO()
     if file_format == "csv":
         df.to_csv(memory_file, index=False)
     elif file_format == "json":
@@ -47,23 +51,37 @@ def dataframe_to_memory_file(df: pd.DataFrame, file_format: str = "csv"):
     return memory_file
 
 
-@app.post("/data-processor/cleanse")
-async def cleanse(dataset: UploadFile):
+# Helper function to post dataset to URL
+async def post_dataset_to_url(df: pd.DataFrame, file_name: str, file_format: str = "csv"):
+    memory_file = dataframe_to_memory_file(df, file_format=file_format)
     try:
-        # Чтение загруженного набора данных
-        df = read_dataset(dataset)
+        target_url = f"{DB_SERVICE_URL}/upload/"
+        async with httpx.AsyncClient() as client:
+            response = await client.post(target_url, files={"file": (f"{file_name}", memory_file, "application/octet-stream")})
+            response.raise_for_status()  # Raise exception for unsuccessful response
+            return response.json()
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(status_code=e.response.status_code, detail=f"Failed to post dataset: {e.response.text}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error posting dataset to target URL: {str(e)}")
 
-        # Удаление строк с пропущенными значениями и дубликатов
+
+@app.post("/data-processor/cleanse")
+async def cleanse(dataset_url: str, ):
+    try:
+        # Download the dataset from the URL
+        df = await download_dataset(dataset_url)
+
+        # Remove rows with missing values and duplicates
         cleansed_data = df.dropna().drop_duplicates()
 
-        # Возвращаем очищенные данные в формате CSV с сохранением всех столбцов
+        # Post the cleansed data to the target URL
+        post_response = await post_dataset_to_url(cleansed_data, dataset_url.split('/')[-1])
+
+        # Return the cleansed data as a CSV file
         file_in_memory = dataframe_to_memory_file(cleansed_data, file_format="csv")
 
-        return StreamingResponse(
-            file_in_memory,
-            media_type="application/octet-stream",
-            headers={"Content-Disposition": 'inline; filename="cleansed_data.csv"'},
-        )
+        return post_response
     except HTTPException as e:
         raise e
     except Exception as e:
@@ -71,24 +89,27 @@ async def cleanse(dataset: UploadFile):
 
 
 @app.post("/data-processor/merge")
-async def merge(dataset_a: UploadFile, dataset_b: UploadFile, merge_field_name: str):
+async def merge(dataset_a_url: str, dataset_b_url: str, merge_field_name: str):
     try:
-        df_a = read_dataset(dataset_a)
-        df_b = read_dataset(dataset_b)
+        # Download both datasets from their respective URLs
+        df_a = await download_dataset(dataset_a_url)
+        df_b = await download_dataset(dataset_b_url)
 
+        # Ensure the merge field exists in both datasets
         if merge_field_name not in df_a.columns or merge_field_name not in df_b.columns:
             raise HTTPException(
                 status_code=400, detail=f"Merge field '{merge_field_name}' not found in one of the datasets."
             )
 
+        # Merge the datasets on the specified field
         merged_data = pd.merge(df_a, df_b, on=merge_field_name, how="inner")
+
+        # Post the merged data to the target URL
+        post_response = await post_dataset_to_url(merged_data, dataset_a_url.split('/')[-1])
+
         file_in_memory = dataframe_to_memory_file(merged_data, file_format="csv")
 
-        return StreamingResponse(
-            file_in_memory,
-            media_type="application/octet-stream",
-            headers={"Content-Disposition": 'inline; filename="merged_data.csv"'},
-        )
+        return post_response
     except HTTPException as e:
         raise e
     except Exception as e:
@@ -96,35 +117,34 @@ async def merge(dataset_a: UploadFile, dataset_b: UploadFile, merge_field_name: 
 
 
 @app.post("/data-processor/anonymize")
-async def anonymize(dataset: UploadFile, anonymization_params: AnonymizationType, column_name: str):
+async def anonymize(dataset_url: str, anonymization_params: AnonymizationType, column_name: str):
     try:
-        df = read_dataset(dataset)
+        # Download the dataset from the URL
+        df = await download_dataset(dataset_url)
 
         # Check if the specified column exists
         if column_name not in df.columns:
             raise HTTPException(status_code=400, detail=f"Column '{column_name}' not found in the dataset.")
 
-        # Apply the anonymization logic to the specified column
+        # Apply anonymization based on the specified type
         if anonymization_params == AnonymizationType.HideEMail:
-            # Replace email addresses (e.g., example@mail.com) with a masked version
+            # Mask email addresses
             df[column_name] = df[column_name].replace(
                 to_replace=r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}", 
                 value="hidden@example.com", 
                 regex=True
             )
-        
         elif anonymization_params == AnonymizationType.HideRandomSymbols:
-            # Replace all non-alphanumeric characters with "*"
+            # Replace non-alphanumeric characters with "*"
             df[column_name] = df[column_name].replace(to_replace=r"[^\w\s]", value="*", regex=True)
+
+        # Post the anonymized data to the target URL
+        post_response = await post_dataset_to_url(df, dataset_url.split('/')[-1])
 
         # Return the anonymized data as a CSV file
         file_in_memory = dataframe_to_memory_file(df, file_format="csv")
 
-        return StreamingResponse(
-            file_in_memory,
-            media_type="application/octet-stream",
-            headers={"Content-Disposition": 'inline; filename="anonymized_data.csv"'},
-        )
+        return post_response
     except HTTPException as e:
         raise e
     except Exception as e:
@@ -132,17 +152,16 @@ async def anonymize(dataset: UploadFile, anonymization_params: AnonymizationType
 
 
 @app.post("/data-processor/transform")
-async def transform(
-    dataset: UploadFile,
-    transformation_type: TransformationType,
-    transformation_column_name: str
-):
+async def transform(dataset_url: str, transformation_type: TransformationType, transformation_column_name: str):
     try:
-        df = read_dataset(dataset)
+        # Download the dataset from the URL
+        df = await download_dataset(dataset_url)
 
+        # Ensure the column exists
         if transformation_column_name not in df.columns:
             raise HTTPException(status_code=400, detail=f"Column '{transformation_column_name}' not found in the dataset.")
 
+        # Apply the specified transformation
         if transformation_type == TransformationType.Uppercase:
             df[transformation_column_name] = df[transformation_column_name].str.upper()
         elif transformation_type == TransformationType.Lowercase:
@@ -152,13 +171,14 @@ async def transform(
         elif transformation_type == TransformationType.Capitalize:
             df[transformation_column_name] = df[transformation_column_name].str.capitalize()
 
+        # Post the transformed data to the target URL
+        post_response = await post_dataset_to_url(df, dataset_url.split('/')[-1])
+
+
+        # Return the transformed data as a CSV file
         file_in_memory = dataframe_to_memory_file(df, file_format="csv")
 
-        return StreamingResponse(
-            file_in_memory,
-            media_type="application/octet-stream",
-            headers={"Content-Disposition": 'inline; filename="transformed_data.csv"'},
-        )
+        return post_response
     except HTTPException as e:
         raise e
     except Exception as e:
--- kubernetes_app/session_service/app.py
+++ kubernetes_app/session_service/app.py
@@ -1,96 +1,287 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException, Path, Body
+import httpx
+import requests
+from io import BytesIO
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Optional, Dict
-from enum import Enum 
+from typing import List, Optional, Dict
+from uuid import uuid4
 
 
-app = FastAPI(
-    title="mETaL - OpenAPI 3.0",
-    description="This is one of mETaL's microservices based on the OpenAPI 3.0 specification.",
-    version="1.0.0",
-)
+dataset_a_url = "https://s666vla.storage.yandex.net/rdisk/73881565a366ba8794a2a93be0cb511773965618b089ed1c5e8f5adee6c42e1f/674f2d6f/GORH-WLDFTp3elLhwAv8recGK_lQ-p9MpCAzIZd89OUHtGCzD2nLBgYKSQW_MXxwX7yvhHLXLsH-hJm_DvUoiQ==?uid=0&filename=dataset_a.csv&disposition=attachment&hash=bJtznY6WJOocHUXbvZzVpCiaIo2LEQCmut9NNFOxT7sPxhxlAoRxPCvla96JpKV9q/J6bpmRyOJonT3VoXnDag%3D%3D&limit=0&content_type=text%2Fplain&owner_uid=1489006053&fsize=52&hid=72b8455668ee2f367d93ffcc383cb5f7&media_type=spreadsheet&tknv=v2&ts=6285fe702f9c0&s=04427bee1826d30cfbbabe42534dda3eed9408ac0153f5ec96fbd6afc4ff43ab&pb=U2FsdGVkX188ixIoil2We54HIqjCEzgr2Wn9du1jmh82LuYEQ9CdhVRaX3O2HK41WkULb52-bpooeQy-Px3y9cV8Yn7Rh-AKYD5eCPtKL5Y"
+dataset_b_url = "https://s50vlx.storage.yandex.net/rdisk/e4e0699f65100fc7dffc7a47e0560337369fef20b212d9f59fdcd142a3aef56c/674f2e83/GORH-WLDFTp3elLhwAv8rQmlKbLQHYt1UW7_-y6QksNtf7YILfar6sFVC7XWPuFsTuZn9j8OdKgMdnjqsMJKWg==?uid=0&filename=dataset_b.csv&disposition=attachment&hash=kUOS5fDnBXOdl8JdecnPw0KicRXsG/1Qink7LW6ZKeVPohlEfEPxnWItCS%2BfDnIpq/J6bpmRyOJonT3VoXnDag%3D%3D&limit=0&content_type=text%2Fplain&owner_uid=1489006053&fsize=85&hid=dbb3ca65a882e72e6c1ca8295aa9497d&media_type=spreadsheet&tknv=v2&ts=6285ff77666c0&s=2953d76e1644fd9afaac04b081b33763b01c1285d512005e80b0c6125213726f&pb=U2FsdGVkX1-4yacjNiy0K5l_GaiwTeF6Q80AqyZtMK7AIdSd68iheXW3kdT-KOajidIvuy19NH04U8lk5uCS0D1J4uE0pTvklaOusDXZFwc"
 
-# Models for request and response schemas
-class SessionStatus(str, Enum):
-    INITIAL = "Initial"
-    READY = "Ready"
-    RUNNING = "Running"
-    SUCCESS = "Success"
-    ERROR = "Error"
-
-class SessionCreateRequest(BaseModel):
-    configId: str
-    processingParameters: Optional[Dict[str, str]] = None
-
-class Session(BaseModel):
-    sessionId: str
-    config: str
-    processingParameters: Optional[Dict[str, str]]
-    status: SessionStatus
-
-class SessionStatusUpdate(BaseModel):
-    status: SessionStatus
+DB_SERVICE_URL = "http://db-service-service:8080"
+EMAIL_SERVICE_URL = "http://email-service:8080"
+DATAPROCESSING_SERVICE_URL = "http://dataprocessing-service:8080"
 
 
-# In-memory database mockup for demonstration
-sessions = {}
+DB_SERVICE_URL = "http://192.168.49.2:31641"
+EMAIL_SERVICE_URL = "http://192.168.49.2:30273"
+DATAPROCESSING_SERVICE_URL = "http://192.168.49.2:32755"
 
+
+app = FastAPI()
+
+
+# Models
+class Step(BaseModel):
+    stepType: str
+    nextStepID: Optional[str] = None
+    prevStepID: Optional[str] = None
+
+    dataset_a_s3_url: str
+    dataset_b_s3_url: Optional[str] = None
+
+    column_name: str
+    step_param: Optional[str] = None
+
+
+class DataProcessorSession(BaseModel):
+    name: str
+    dataset_a_download_url: str
+    dataset_b_download_url: str
+    steps: List[Step]
+
+
+class DataProcessorSessionResponse(BaseModel):
+    id: str
+    name: str
+    steps: List[Step]
+
+
+class StepEndWebhookRequest(BaseModel):
+    stepId: str
+    status: str
+
+
+# In-memory storage
+sessions: Dict[str, DataProcessorSession] = {}
+
+# HTTP client for data processor interactions
+dataprocessing_client = httpx.AsyncClient(
+    base_url=DATAPROCESSING_SERVICE_URL
+)  # Adjust base URL if needed
+
+email_client = httpx.AsyncClient(
+    base_url=EMAIL_SERVICE_URL
+)  # Adjust base URL if needed
+
+db_service_client = httpx.AsyncClient(
+    base_url=DB_SERVICE_URL
+)  # Adjust base URL if needed
+
+
+async def upload_file_to_db_service(file_url: str):
+
+    
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(file_url)
+            response.raise_for_status()  # Raise error if the request failed
+
+            # Save file in memory as a BytesIO object
+            file_in_memory = BytesIO(response.content)
+            file_in_memory.seek(0)  # Reset the pointer to the beginning of the file
+
+            # Step 2: Prepare the file to be uploaded
+            files = {
+                "file": (f"{uuid4()}.txt", file_in_memory, "application/octet-stream")
+            }
+
+            # Step 3: Post the file to your FastAPI server
+            upload_response = await client.post(
+                f"{DB_SERVICE_URL}/upload/", files=files
+            )
+            upload_response.raise_for_status()  # Raise error if the upload failed
+
+            # Handle the response from your FastAPI server
+
+            dataset_url_at_s3 = await upload_response.json()
+            dataset_url_at_s3 = dataset_url_at_s3.get("url")
+
+            result = upload_response.json()
+            print(f"File uploaded successfully! URL: {result['url']}")
+
+            return dataset_url_at_s3
+        except httpx.RequestError as e:
+            print(f"Error downloading or uploading the file: {e}")
+        except httpx.HTTPStatusError as e:
+            print(f"HTTP error occurred: {e}")
+        
 
 # Endpoints
-@app.post("/sessions", response_model=Session, status_code=201)
-async def create_session(request: SessionCreateRequest = Body(...)):
-    """Create a new session."""
-    new_session_id = f"session_{len(sessions) + 1}"
-    session = Session(
-        sessionId=new_session_id,
-        config=request.configId,
-        processingParameters=request.processingParameters,
-        status=SessionStatus.INITIAL
+@app.post(
+    "/data-processor-sessions",
+    response_model=DataProcessorSessionResponse,
+    status_code=201,
+)
+async def create_session(session: DataProcessorSession):
+    session_id = str(uuid4())
+    session_data = DataProcessorSessionResponse(
+        id=session_id, name=session.name, steps=session.steps
     )
-    sessions[new_session_id] = session
-    return session
+    sessions[session_id] = session_data
+    return session_data
 
 
-@app.get("/sessions/{sessionId}", response_model=Session)
-async def get_session(sessionId: str = Path(..., description="Unique session ID")):
-    """Get session details by ID."""
-    session = sessions.get(sessionId)
-    if not session:
-        raise HTTPException(status_code=404, detail="Session not found")
-    return session
+@app.get("/data-processor-sessions", response_model=List[DataProcessorSessionResponse])
+async def get_sessions():
+    return list(sessions.values())
 
 
-@app.delete("/sessions/{sessionId}", status_code=204)
-async def delete_session(sessionId: str = Path(..., description="Unique session ID")):
-    """Delete a session by ID."""
+@app.get(
+    "/data-processor-sessions/{sessionId}", response_model=DataProcessorSessionResponse
+)
+async def get_session(sessionId: str):
     if sessionId not in sessions:
         raise HTTPException(status_code=404, detail="Session not found")
-    del sessions[sessionId]
-    return
+    return sessions[sessionId]
 
 
-@app.get("/sessions/{sessionId}/status", response_model=SessionStatus)
-async def get_session_status(sessionId: str = Path(..., description="Unique session ID")):
-    """Get the status of a session."""
-    session = sessions.get(sessionId)
-    if not session:
+@app.put(
+    "/data-processor-sessions/{sessionId}", response_model=DataProcessorSessionResponse
+)
+async def update_session(sessionId: str, session: DataProcessorSession):
+    if sessionId not in sessions:
         raise HTTPException(status_code=404, detail="Session not found")
-    return session.status
+    updated_session = DataProcessorSessionResponse(
+        id=sessionId, name=session.name, steps=session.steps
+    )
+    sessions[sessionId] = updated_session
+    return updated_session
 
 
-@app.put("/sessions/{sessionId}/status", response_model=SessionStatus)
-async def update_session_status(
-    sessionId: str = Path(..., description="Unique session ID"),
-    status_update: SessionStatusUpdate = Body(...)
-):
-    """Update the status of a session."""
-    session = sessions.get(sessionId)
-    if not session:
+@app.delete(
+    "/data-processor-sessions/{sessionId}", response_model=DataProcessorSessionResponse
+)
+async def delete_session(sessionId: str):
+    if sessionId not in sessions:
         raise HTTPException(status_code=404, detail="Session not found")
-    session.status = status_update.status
-    return session.status
+    session = sessions.pop(sessionId)
+    return session
 
+
+@app.post("/data-processor-sessions/{sessionId}/start", status_code=200)
+async def start_session(sessionId: str):
+    failed_somewhere = False
+
+    if sessionId not in sessions:
+        raise HTTPException(status_code=404, detail="Session not found")
+
+    session = sessions[sessionId]
+    step_results = []
+
+    dataset_a_url_s3 = upload_file_to_db_service(session.dataset_a_download_url)
+    dataset_b_url_s3 = upload_file_to_db_service(session.dataset_b_download_url)
+
+    # Process each step in the session
+    for step in session.steps:
+        if step.stepType == "Cleansing":
+            try:
+                # Send to the cleansing API
+                cleanse_response = await dataprocessing_client.post(
+                    "/data-processor/cleanse", files={"dataset": dataset_a_url_s3}
+                )
+                step_results.append(
+                    {"stepId": step.stepType, "status": "Completed", "message": "okay"}
+                )
+
+            except Exception as e:
+                step_results.append(
+                    {"stepId": step.stepType, "status": "Error", "message": str(e)}
+                )
+                failed_somewhere = True
+
+        elif step.stepType == "Anonymization":
+            try:
+
+                # Send to the anonymization API
+                anonymize_response = await dataprocessing_client.post(
+                    "/data-processor/anonymize",
+                    files={"dataset": dataset_a_url_s3},
+                    json={
+                        "anonymization_params": step.step_param,
+                        "column_name": step.column_name,
+                    },
+                )
+                step_results.append(
+                    {"stepId": step.stepType, "status": "Completed", "message": "okay"}
+                )
+
+            except Exception as e:
+                step_results.append(
+                    {"stepId": step.stepType, "status": "Error", "message": str(e)}
+                )
+                failed_somewhere = True
+
+        elif step.stepType == "Transformation":
+            try:
+
+                # Send to the transformation API
+                transform_response = await dataprocessing_client.post(
+                    "/data-processor/transform",
+                    files={"dataset": dataset_a_url_s3},
+                    json={
+                        "transformation_type": step.step_param,
+                        "transformation_column_name": step.column_name,
+                    },
+                )
+                step_results.append(
+                    {"stepId": step.stepType, "status": "Completed", "message": "okay"}
+                )
+
+            except Exception as e:
+                step_results.append(
+                    {"stepId": step.stepType, "status": "Error", "message": str(e)}
+                )
+                failed_somewhere = True
+
+        elif step.stepType == "Merge":
+            try:
+
+                # Send to the merge API
+                merge_response = await dataprocessing_client.post(
+                    "/data-processor/merge",
+                    files={
+                        "dataset_a": dataset_a_url_s3,
+                        "dataset_b": dataset_b_url_s3,
+                    },
+                    json={"merge_field_name": step.column_name},
+                )
+                step_results.append(
+                    {"stepId": step.stepType, "status": "Completed", "message": "okay"}
+                )
+
+            except Exception as e:
+                step_results.append(
+                    {"stepId": step.stepType, "status": "Error", "message": str(e)}
+                )
+                failed_somewhere = True
+
+    step_results_str = str(step_results)
+    if not failed_somewhere:
+        email_notification = {
+            "session_id": sessionId,
+            "email": f"session has been finished succesfuly: {step_results_str}",
+            "new_status_id": 0,
+        }
+
+    else:
+        email_notification = {
+            "session_id": sessionId,
+            "email": f"session has been failed succesfuly: {step_results_str}",
+            "new_status_id": 0,
+        }
+
+    upload_response = requests.post(
+        f"{EMAIL_SERVICE_URL}/session/notify", data=email_notification
+    )
+
+    return {"sessionId": sessionId, "stepResults": step_results}
+
+
+# Run the application with: uvicorn app_name:app --reload
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
